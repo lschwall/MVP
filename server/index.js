973a7ed3ddd433e62fb0ce5751325865fa4c3261
Larry Schwall iv <larryschwalliv@Larrys-MBP.attlocal.net>
2020-09-22 00:15:33 -0500

    get/post works with test data. working on react components

--- server/index.js
+++ server/index.js
@@ -1,18 +1,22 @@
 require('dotenv').config();
 const { PORT } = process.env;
+const path = require('path')
 const express = require('express')
 const bodyparser = require('body-parser');
-const app = express()
-const { db } = require('./db');
-const Movie = require('./db');
+const app = express();
+const { db } = require('../server/db');
+const Movie = require('./db')
+
+app.use(express.static('dist'));
+
+
 
 app.use(bodyparser.urlencoded({extended: true}))
-app.listen(PORT, () => {
-    console.log(`LISTENING ON ${PORT}`)
-})
 
-app.get('/', (req, res) => {
+
+app.get('/movies', (req, res) => {
     Movie.find()
+        //result is the result of the entire query
     .then(result => {
         console.log('MOVIE FOUND!')
         res.status(200).send(result)
@@ -22,13 +26,14 @@ app.get('/', (req, res) => {
     })
 })
 
-app.post('/',(req, res) => {
+
+app.post('/movies',(req, res) => {
     const testMovie = { 
-        id: 1212,
-        original_title: 'The Santa Clause',
-        title: 'The Santa Clause',
-        vote_average: 2,
-        personal_rating: 2
+        id: 1214,
+        original_title: 'FAST 7',
+        title: 'FAST 7',
+        vote_average: 1.3,
+        personal_rating: 0.4
     }
 
     Movie.find(testMovie)
@@ -39,15 +44,27 @@ app.post('/',(req, res) => {
                     .then((savedMovie) => {
                         res.status(200)
                         res.send(savedMovie)
+                        res.send('MOVIE SAVED!')
                         return
                     })
             }else{
                 console.log('MOVIE ALREADY ADDED')
                 res.status(302)
-                res.send()
+                res.send('MOVIE ALREADY IN DATABASE, PLEASE SEARCH AGAIN')
             }
         })
         .catch((err) => {
             console.log('ERROR IN SAVING NEW TITLE', err)
         })
+})
+
+
+// NOTHING BELOW THIS LINE
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+);
+
+app.listen(PORT, () => {
+    console.log(`LISTENING ON ${PORT}`)
 })
---
+++ src/index.js
@@ -0,0 +1,10 @@
+/*
+INDEX IS A FUNCTIONAL COMPONENT THAT RETURNS A REACT ELEMENT.
+*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+const Index = () => {
+  return <div>Welcome to React!</div>;
+};
+ReactDOM.render(<Index />, document.getElementById('root'));
